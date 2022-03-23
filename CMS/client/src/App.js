1d9b6c60af321d74db28d2c4d207426fc3d43381
Yash Dhameliya <yashdhameliya39@gmail.com>
2022-03-23 12:56:59 +0530

    Banner Added

--- CMS/client/src/App.js
+++ CMS/client/src/App.js
@@ -1,8 +1,18 @@
+
+import React from 'react';
+import {Box } from '@material-ui/core';
+
 import Header from './components/Header'; // Nevigate Home from components Folder
+import Home from './components/home/Home';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Box style={{marginTop: 64}}>
+        <Home />
+      </Box>
+    </>
   );
 }
 
---
+++ CMS/client/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+
+import Banner from './Banner'; //Use components of Banner
+
+
+const Home = () =>{
+    return(
+        <Banner/>
+    )
+}
+
+export default Home;
