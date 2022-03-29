db5b397edd48053a625b411c89191f4271ff5a92
Yash Dhameliya <yashdhameliya39@gmail.com>
2022-03-29 14:47:58 +0530

    Posts modual Updated

--- CMS/client/src/components/home/Posts.jsx
+++ CMS/client/src/components/home/Posts.jsx
@@ -1,10 +1,17 @@
 
+import { Grid } from "@material-ui/core";
 
-
+import Post from "./Post";
 
 const Posts = () =>{
+    let Posts = [1,2,3,4,5,6,7,8,9];
     return(
-        <p>Posts 123</p>
+        Posts.map(post => (
+            <Grid item lg={3} sm={4} xs={12}>
+                <Post/>
+            </Grid>
+        ))
+
     )
 }
 
