c727735681fad4839d16aebf15fd79e34dd078a9
Yash Dhameliya <yashdhameliya39@gmail.com>
2022-04-02 16:05:43 +0530

    Posts components create

---
+++ CMS/react-blog/src/Components/Posts/Post/SinglePostDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SinglePostDetails() {
+  return (
+    <div>
+        <h1>Single Post Components</h1>
+    </div>
+  )
+}
---
+++ CMS/react-blog/src/Components/Posts/Post/style.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function style() {
+  return (
+    <div>
+        <h1>Style Components</h1>
+    </div>
+  )
+}
