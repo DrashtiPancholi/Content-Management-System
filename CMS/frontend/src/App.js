90f7cc07f3217c74d596328a50c9d97e6b83ed3f
Yash Dhameliya <yashdhameliya39@gmail.com>
2022-04-03 00:07:00 +0530

    Login Frontend

--- CMS/frontend/src/App.js
+++ CMS/frontend/src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import Navigation from './components/Navigation';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navigation />
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />}/>
       </Routes>
     </BrowserRouter>
   );
