8ddc09353ba8ebc24972575cb564877c1bb1c2d7
Yash Dhameliya <yashdhameliya39@gmail.com>
2022-04-11 21:59:29 +0530

    DB Connection Changed

--- CMS/backend/connection.js
+++ CMS/backend/connection.js
@@ -1,5 +1,13 @@
+// const mongoose = require('mongoose');
+// require('dotenv').config()
+// mongoose.connect('mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cms.qgsn3.mongodb.net/cms?retryWrites=true&w=majority', ()=>{
+//     console.log('connected to db')
+// })
+
 const mongoose = require('mongoose');
 require('dotenv').config()
-mongoose.connect('mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cms.qgsn3.mongodb.net/cms?retryWrites=true&w=majority', ()=>{
+mongoose.connect('mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.y71yu.mongodb.net/cms?retryWrites=true&w=majority', ()=>{
     console.log('connected to db')
 })
+
+// mongodb+srv://root:<password>@cluster0.y71yu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
