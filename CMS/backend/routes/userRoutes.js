d2a37c427c2d0ba7e56e794e1a4de56a758da3f5
Drashati Pancholi <18bmiit024@gmail.com>
2022-04-12 19:35:04 +0100

    update routes

--- CMS/backend/routes/userRoutes.js
+++ CMS/backend/routes/userRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const authuser = require('../middleware/auth');
 const user = require('../models/user');
 
 //user creation
@@ -8,8 +9,8 @@ router.post('/', async(req, res)=>{
 
     try{
         const user = await User.create({email, passwprd});
-        await user.generateAuthToken()
-        res.status(201).json(user);
+        const token = await user.generateAuthToken();
+        res.status(201).json({user, token});
     }
      catch(error) {
          let msg;
@@ -30,11 +31,25 @@ router.post('/login',async(req, res)=>{
     const {email, password} = req.body;
     try{
         const use = await User.findByCredentials(email, password);
-        await user.generateAuthToken();
+        const token = await user.generateAuthToken();
        // console.log(user)
-        res.json(user)
+        res.json({user, token});
 
     }catch(e){
+        res.status(400).json(e.message)
+    }
+})
+
+//logout user
+
+router.delete('/logout', authuser, async(req, res)=>{
+    try{
+        req.user.tokens = req.user.tokens.filter((tokenObj)=>{
+            return tokenObj.token != req.token;
+        })
+        await req.user.save();
+        res.status(200).send()
+    } catch(e) {
         res.status(400).json(e.message)
     }
 })
